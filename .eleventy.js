1d8f474d44272799d63acbad88acd2d21f9bb26f
gecko655 <6166778+gecko655@users.noreply.github.com>
2025-05-05 22:42:54 +0900

    11ty の導入

---
+++ .eleventy.js
@@ -0,0 +1,32 @@
+const prettier = require("prettier");
+
+const passThroughPaths = ["src/js", "src/css", "src/images", "src/favicon.ico", "src/apple-touch-icon.png", "src/CNAME"];
+
+module.exports = function (eleventyConfig) {
+  for (const path of passThroughPaths) {
+    eleventyConfig.addPassthroughCopy(path);
+  }
+
+  // https://bnijenhuis.nl/notes/adding-prettier-in-eleventy-using-transforms/
+  eleventyConfig.addTransform("prettier", async (content, outputPath) => {
+    if (outputPath?.endsWith(".html")) {
+      return prettier.format(content, {
+        parser: "html",
+        printWidth: 500,
+        tabWidth: 2,
+        lineEnding: "\n",
+        useTabs: false,
+      });
+    }
+    return content;
+  });
+  return {
+    dir: {
+      input: "src",
+      includes: "../_includes",
+      output: "dist",
+    },
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk",
+  };
+};
